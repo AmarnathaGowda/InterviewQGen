db6c6ea512a1fd7f5d678b6462f2ce05f87c69a8
AmarnathaGowda <amarnath1413@gmail.com>
2024-06-11 17:46:38 +0530

    initial setup and folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup (
+    name= "InterViewQGen",
+    version= "0.0.1",
+    author= "Amarnatha Gowda",
+    author_email="amarnath1413@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
+
